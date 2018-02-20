42d7f301ff8a111c014726f09370aa9e051dfdf7
asb346 <asb346@drexel.edu>
2018-02-20 15:11:06 -0500

    title and help screens

---
+++ gmap345/Assets/script/TitleGM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleGM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("game", LoadSceneMode.Single);
+            print("You hit space");
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SceneManager.LoadScene("help", LoadSceneMode.Single);
+            print("You hit h");
+        }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            SceneManager.LoadScene("title", LoadSceneMode.Single);
+            print("You hit backspace");
+        }
+    }
+}
