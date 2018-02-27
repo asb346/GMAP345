1ca97b96a12d8c10ad0c06bb85602adddc6f80c8
asb346 <asb346@drexel.edu>
2018-02-27 17:25:21 -0500

    enemy movements better

---
+++ gmap345/Assets/script/EnemyRLStrafe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRLStrafe : MonoBehaviour {
+
+    public float speed = 2f;
+    public Vector2 direction = Vector2.left;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (direction==Vector2.left)
+        {
+            Debug.Log("switch");
+            direction = Vector2.right;
+        }
+        else
+        {
+            Debug.Log("switch");
+            direction = Vector2.left;
+        }
+    }
+}
---
+++ gmap345/Assets/script/EnemyUDStrafe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUDStrafe : MonoBehaviour {
+
+    public float speed = 2f;
+    public Vector2 direction = Vector2.up;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (direction == Vector2.up)
+        {
+            Debug.Log("switch");
+            direction = Vector2.down;
+        }
+        else
+        {
+            Debug.Log("switch");
+            direction = Vector2.up;
+        }
+    }
+}
