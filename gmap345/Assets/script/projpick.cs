f8ed4e4f2ad05f3981d64c774d363c76f9a32b9d
863867593 <lishun2040@sina.com>
2018-03-12 09:25:47 -0400

    updated
    except rm4

---
+++ gmap345/Assets/script/projpick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projpick : MonoBehaviour {
+    public GameObject proj;
+    public float attackspeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.gameObject.CompareTag("player"))
+        {
+            if (collision.gameObject.GetComponents<patk1>().Length > 0)
+            {
+               
+            }
+            else {
+                collision.gameObject.AddComponent<patk1>();
+                var p = collision.GetComponent<patk1>();
+                p.p = proj;
+                p.atkspd = attackspeed;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
