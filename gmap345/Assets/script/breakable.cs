b4229f27f0f3661d27f4d750656b89f93d33f38c
863867593 <lishun2040@sina.com>
2018-03-12 15:56:31 -0400

    breakable walls

---
+++ gmap345/Assets/script/breakable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.gameObject.CompareTag("projectile"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
