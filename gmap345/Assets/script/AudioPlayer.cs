08ceaaa6bb01c97e20cec10d27d04afddd8ae9ae
VSantillo <vincentsantillo@gmail.com>
2018-03-13 17:49:33 -0400

    Fuck Shit.

---
+++ gmap345/Assets/script/AudioPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPlayer : MonoBehaviour {
+
+	void Awake ()
+    {
+    	GameObject[] objs = GameObject.FindGameObjectsWithTag("Audio Source");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+ }
