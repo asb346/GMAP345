d0ac5c535de01ba18ba6948ffdbc09105f43b3c7
asb346 <asb346@drexel.edu>
2018-03-12 22:36:18 -0400

    fixed some stuff, changed camera...

--- gmap345/Assets/script/EnemyRLStrafe.cs
+++ gmap345/Assets/script/EnemyRLStrafe.cs
@@ -6,19 +6,23 @@ public class EnemyRLStrafe : MonoBehaviour {
 
     public float speed = 2f;
     public Vector2 direction = Vector2.left;
+
+    public Rigidbody2D rb;
+
     // Use this for initialization
     void Start () {
-		
-	}
+        rb = transform.GetComponent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        rb.velocity = direction * speed;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (direction==Vector2.left)
+        if (direction == Vector2.left)
         {
             direction = Vector2.right;
         }
@@ -27,4 +31,5 @@ public class EnemyRLStrafe : MonoBehaviour {
             direction = Vector2.left;
         }
     }
+
 }
