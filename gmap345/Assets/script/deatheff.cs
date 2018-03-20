1a0e67f92c69b45d38fb6378ac6a75fbfb6d3dcd
863867593 <lishun2040@sina.com>
2018-03-20 14:59:04 -0400

    deatheffect

---
+++ gmap345/Assets/script/deatheff.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deatheff : MonoBehaviour {
+    public float delay;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("destroy");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+    }
+}
