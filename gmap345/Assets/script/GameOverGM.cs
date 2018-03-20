a7c225c2dee84984277123a7df15138665c85ec0
VSantillo <vincentsantillo@gmail.com>
2018-03-20 15:38:25 -0400

    Game Over Music should reset now.

--- gmap345/Assets/script/GameOverGM.cs
+++ gmap345/Assets/script/GameOverGM.cs
@@ -8,13 +8,16 @@ public class GameOverGM : MonoBehaviour {
 
     public Button restartButton;
     public Button exitButton;
-    public AudioSource music;
+    private AudioSource music;
     public AudioClip newClip;
+    private AudioClip loopClip;
 
     // Use this for initialization
     void Start () {
         restartButton.onClick.AddListener(restartgame);
         exitButton.onClick.AddListener(exitgame);
+        music = (AudioSource)FindObjectOfType(typeof(AudioSource));
+        loopClip = music.clip;
         music.Stop();
         music.loop = false;
         music.clip = newClip;
@@ -24,6 +27,9 @@ public class GameOverGM : MonoBehaviour {
     void restartgame()
     {
         music.Stop();
+        music.clip = loopClip;
+        music.loop = true;
+        music.Play();
         SceneManager.LoadScene("int", LoadSceneMode.Single);
     }
     void exitgame()
