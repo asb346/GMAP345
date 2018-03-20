d427ecb82303f06c196df1cf4e38c63422f9e1a2
VSantillo <vincentsantillo@gmail.com>
2018-03-19 22:23:47 -0400

    Title Scene Changes
    For Title Scene, modified “TitleGM” to allow for direct control over
    the volume of the BGM. GM game object now takes in an audio source
    object and a float and that will be the final value.

--- gmap345/Assets/script/TitleGM.cs
+++ gmap345/Assets/script/TitleGM.cs
@@ -9,16 +9,19 @@ public class TitleGM : MonoBehaviour {
     public Button startButton;
     public Button helpButton;
     public Button creditsButton;
+    public AudioSource audioSource;
+    public float  finalVolume;
 
 	// Use this for initialization
 	void Start () {
         startButton.onClick.AddListener(startgame);
         helpButton.onClick.AddListener(loadhelp);
 	}
-	
+
 
     void startgame()
     {
+        audioSource.volume = finalVolume;
         SceneManager.LoadScene("int", LoadSceneMode.Single);
     }
 
