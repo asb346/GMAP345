38dbe4b5e8f61897b1a21d5f03b6028811b3125f
863867593 <lishun2040@sina.com>
2018-03-20 12:00:13 -0400

    push

--- gmap345/Assets/script/projs.cs
+++ gmap345/Assets/script/projs.cs
@@ -7,29 +7,40 @@ public class projs : MonoBehaviour {
     public float speed;
     public int damage;
     public float eff;
+    public float travel;
+
     public void Start()
     {
-        
+        StartCoroutine("destroy");
         rig = GetComponent<Rigidbody2D>();
         rig.velocity= new Vector2(-1*speed * Mathf.Sin(Mathf.Deg2Rad*rig.transform.rotation.eulerAngles.z), speed * Mathf.Cos(Mathf.Deg2Rad*rig.transform.rotation.eulerAngles.z));
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("wall")) {
+        if (collision.gameObject.CompareTag("box")) {
+            
             Destroy(this.gameObject);
+
+        }
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            Destroy(this.gameObject);
+
         }
         if (collision.gameObject.CompareTag("enemy"))
         {
-            if (collision.gameObject.GetComponents<ustat>().Length > 0) {
-                collision.gameObject.GetComponent<ustat>().damage(damage);
-            }
+
             collision.GetComponent<Rigidbody2D>().AddForce(GetComponent<Rigidbody2D>().velocity*eff);
             Destroy(this.gameObject);
         }
 
     }
-
+    private IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(travel);
+        Destroy(this.gameObject);
+    }
     public void FixedUpdate()
     {
       
